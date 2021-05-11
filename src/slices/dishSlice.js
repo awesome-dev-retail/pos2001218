874d4adbabac281ce64b3a66db3c70229b2e1562
cm2001218 <cm2001218@gmail.com>
2021-05-12 08:05:23 +1200

    0512 update

--- src/slices/dishSlice.js
+++ src/slices/dishSlice.js
@@ -26,7 +26,7 @@ export const fetchDishListInShop = createAsyncThunk("dish/fetchDishListInShop", 
   try {
     const res = await axios({
       url: `https://pos-restaurant-be-dev.azurewebsites.net/pos/data/dish/list_in_shop?shopId=${id}`,
-      headers: { Authorization: "Bearer i0cIuZhvtjrU5RYSaaAmmw==" },
+      headers: { Authorization: "Bearer LtCCxMZRBmbqbDpqBDlB7Q==" },
     });
     if (res.error) throw res.error;
     console.log("fetchDishListInShop--------------", res);
@@ -42,7 +42,7 @@ export const fetchDishListInMenu = createAsyncThunk("dish/fetchDishListInMenu", 
     console.log("-------menuId-------", menuId);
     const res = await axios({
       url: `https://pos-restaurant-be-dev.azurewebsites.net/pos/data/dish/list_in_shop?classId=${menuId}`,
-      headers: { Authorization: "Bearer i0cIuZhvtjrU5RYSaaAmmw==" },
+      headers: { Authorization: "Bearer LtCCxMZRBmbqbDpqBDlB7Q==" },
     });
     if (res.error) throw res.error;
     console.log("fetchDishListInMenu--------------", res);
@@ -58,7 +58,7 @@ export const saveDish = createAsyncThunk("dish/saveDish", async (dishObj, { reje
     const res = await axios({
       method: "post",
       url: "https://pos-restaurant-be-dev.azurewebsites.net/pos/data/dish/save",
-      headers: { Authorization: "Bearer i0cIuZhvtjrU5RYSaaAmmw==" },
+      headers: { Authorization: "Bearer LtCCxMZRBmbqbDpqBDlB7Q==" },
       data: dishObj,
     });
     if (res.error) throw res.error;
@@ -74,7 +74,7 @@ export const deleteDish = createAsyncThunk("dish/deleteDish", async (id, { rejec
     const res = await axios({
       method: "delete",
       url: `https://pos-restaurant-be-dev.azurewebsites.net/pos/data/dish/delete/${id}`,
-      headers: { Authorization: "Bearer i0cIuZhvtjrU5RYSaaAmmw==" },
+      headers: { Authorization: "Bearer LtCCxMZRBmbqbDpqBDlB7Q==" },
     });
     if (res.error) throw res.error;
     console.log("deleteDish--------------", res);
--- src/slices/invoiceSlice.js
+++ src/slices/invoiceSlice.js
@@ -28,7 +28,7 @@ export const calculateInvoice = createAsyncThunk("invoice/calculateInvoice", asy
     const res = await axios({
       method: "post",
       url: "https://pos-restaurant-be-dev.azurewebsites.net/pos/data/invoice/calculate",
-      headers: { Authorization: "Bearer i0cIuZhvtjrU5RYSaaAmmw==" },
+      headers: { Authorization: "Bearer LtCCxMZRBmbqbDpqBDlB7Q==" },
       data: invoice,
     });
     if (res.error) throw res.error;
