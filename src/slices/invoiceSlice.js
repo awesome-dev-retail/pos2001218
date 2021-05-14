174d78f206e8ae9a06b92d50363076747c7f9ce6
Kevin <cm2001218@gmail.com>
2021-05-15 09:48:45 +1200

    update token

--- src/slices/invoiceSlice.js
+++ src/slices/invoiceSlice.js
@@ -28,7 +28,7 @@ export const calculateInvoice = createAsyncThunk("invoice/calculateInvoice", asy
     const res = await axios({
       method: "post",
       url: "https://pos-restaurant-be-dev.azurewebsites.net/pos/data/invoice/calculate",
-      headers: { Authorization: "Bearer XCkfD775gu0EBNuC3FjigQ==" },
+      headers: { Authorization: "Bearer xKSugVriO0BABrQs7LyRmw==" },
       data: invoice,
     });
     if (res.error) throw res.error;
